b03a04ff16a22fc01e0c27b71460950a85db1e75
habeeb makusota <habeebmakusota@habeebs-MacBook-Pro.local>
2024-08-31 17:20:26 +0100

    line logic on x and y axis

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -2,6 +2,7 @@
 
 import 'dart:math';
 
+import 'package:dot_and_boxes/entity/line_entity.dart';
 import 'package:flutter/material.dart';
 
 class GamePage extends StatefulWidget {
@@ -16,6 +17,35 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+
+  bool turnRed = true;
+  List<LineEntity> lines = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (var y = 0; y < widget.dots; y++) {
+      for (var x = 0; x < widget.dots - 1; x++) {
+        lines.add(LineEntity(
+          p1: Point(x, y),
+          p2: Point(x + 1, y),
+        ));
+      }
+
+      if(y != widget.dots - 1) {
+        for (var x = 0; x < widget.dots; x++) {
+          lines.add(LineEntity(
+            p1: Point(x, y),
+            p2: Point(x, y + 1),
+          ));
+        }
+      }
+    }
+    print("lines.length = ${lines.length}"); 
+    print(lines);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,28 +80,56 @@ class _GamePageState extends State<GamePage> {
                         children: [
                           Row(
                             children: List.generate(xDirectionWidgetsCount, (iX) {
-                              return iX.isEven ? Container(
-                                width: circleSquareRib,
-                                height: circleSquareRib,
-                                decoration: const BoxDecoration(
-                                  color: Colors.green,
-                                  shape: BoxShape.circle,
-                                ),
-                              ) : Expanded(
-                                child: Container(
-                                  width: double.infinity,
+
+                              if(iX.isEven){
+                                return Container(
+                                  width: circleSquareRib,
                                   height: circleSquareRib,
-                                  color: Colors.blue,
+                                  decoration: const BoxDecoration(
+                                    color: Colors.green,
+                                    shape: BoxShape.circle,
+                                  ),
+                                );
+                              }
+
+                              final int p1X = (iX - 1) ~/ 2;
+                              final LineEntity line = lines.firstWhere(
+                                (element) => element == LineEntity(p1: Point(p1X, iY), p2: Point(p1X + 1, iY)));
+                              return Expanded(
+                                child: InkWell(
+                                  onTap: () {
+                                    draw(line);
+                                  },
+                                  child: Container(
+                                    width: double.infinity,
+                                    height: circleSquareRib,
+                                    color: line.completed ? Colors.grey.shade800 : null,
+                                  ),
                                 ),
                               );
                             }),
                           ),
                           if(iY != widget.dots - 1) Row(children: List.generate(xDirectionWidgetsCount, (iX) {
-                            return iX.isEven ? Container(
-                              width: circleSquareRib,
-                              height: homeRib,
-                              color: Colors.blue,
-                            ) : Expanded(
+                            
+                            final int p1X = iX ~/ 2;
+                            final LineEntity line = lines.firstWhere(
+                              (element) => element == LineEntity(p1: Point(p1X, iY), p2: Point(p1X, iY + 1)));
+
+                            if(iX.isEven) {
+                              return InkWell(
+                              onTap: () {
+                                draw(line);
+                              },
+                              child: Container(
+                                width: circleSquareRib,
+                                height: homeRib,
+                                color: line.completed ? Colors.grey.shade800 : null,
+                              ),
+                            );
+                            }
+                            
+                            
+                            return Expanded(
                               child: Container(
                                 width: homeRib,
                                 height: homeRib,
@@ -96,5 +154,19 @@ class _GamePageState extends State<GamePage> {
         ],
       ),
     );
+  }
+  
+  void draw(LineEntity line) {
+    if(line.completed) {
+      return;
+    } 
+
+    Color color = turnRed ? Colors.red: Colors.blue;
+    setState(() {
+      line.color = color;
+      line.completed = true;
+    });
+
+    /// todo: add check Completed Home method
   }
 }
